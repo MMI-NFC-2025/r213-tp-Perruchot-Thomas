7b78ccde4afbb9d0985206bdb849691f364b8535
Perruchot-Thomas <thomas.perruchot@edu.univ-fcomte.fr>
2026-01-30 14:38:54 +0100

    image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
@@ -11,5 +9,15 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  adapter: vercel()
+  adapter: vercel(),
+
+  image: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8090', 
+      },
+    ],
+  },
 });
