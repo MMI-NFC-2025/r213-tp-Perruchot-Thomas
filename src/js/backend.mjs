0af99a34f0c0dcd2d85c6c60b6506438e8fee55a
Perruchot-Thomas <thomas.perruchot@edu.univ-fcomte.fr>
2026-03-06 14:40:13 +0100

    favori

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -4,7 +4,7 @@ const db = new PocketBase("http://127.0.0.1:8090");
 
 export async function getOffres() {
     try {
-        let data = await db.collection('Maison').getFullList({
+        let data = await db.collection('maison').getFullList({
             sort: '-created',
         });
         return data;
@@ -40,10 +40,9 @@ export async function getOffresParSurface(surfaceMin) {
     }
 }
 
-
 export async function addOffre(house) {
     try {
-        await db.collection('Maison').create(house);
+        await db.collection('maison').create(house);
         return {
             success: true,
             message: 'Offre ajoutée avec succès'
@@ -54,5 +53,66 @@ export async function addOffre(house) {
             success: false,
             message: 'Une erreur est survenue en ajoutant la maison'
         };
+    }
+}
+
+export async function filterByPrix(minPrix, maxPrix) {
+    try {
+        const data = await db.collection('maison').getFullList({
+            filter: `prix >= ${minPrix} && prix <= ${maxPrix}`,
+            sort: '-prix',
+        });
+        return data;
+    } catch (error) {
+        console.log('Une erreur est survenue lors du filtrage par prix', error);
+        return [];
+    }
+}
+
+export async function setFavori(house) {
+    try {
+        await db.collection('maison').update(house.id, {
+            favori: !house.favori
+        });
+    } catch (error) {
+        console.log('Erreur lors de la mise à jour du favori', error);
+    }
+}
+
+
+
+export async function getAgents() {
+    try {
+        const agents = await db.collection('Agent').getFullList();
+        return agents;
+    } catch (error) {
+        console.error("Erreur agents:", error);
+        return [];
+    }
+}
+
+export async function getAgent(id) {
+    try {
+        const agent = await db.collection('Agent').getOne(id, {
+            expand: 'relation', 
+        });
+        return agent;
+    } catch (error) {
+        console.error("Erreur agent ID:", error);
+        return null;
+    }
+}
+
+
+export async function allArtists() {
+    try {
+
+        const artists = await db.collection("artists").getFullList({
+            sort: 'last_name',
+        });
+        return artists;
+    } catch (error) {
+        console.error("Erreur lors de la récupération des artistes :", error);
+        return []; 
     }
 }
